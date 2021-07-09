2bf69d01339fcd67924f5b71c8ed057c7be9ce81
Juliana Fargnoli <fargnoli.jm@gmail.com>
2021-07-09 07:00:46 -0600

    Submit book functionality

--- src/book.js
+++ src/book.js
@@ -1,12 +1,39 @@
-function createTitle() {
-  
-}
+function createTitle(title) {
+  return `The ${title}`;
+};
+
+function buildMainCharacter(name, age, pronouns) {
+  return {name: name, age: age, pronouns: pronouns};
+};
+
+function saveReview(review, reviews) {
+  if(!reviews.includes(review)) {
+    reviews.push(review);
+  }
+};
+
+function calculatePageCount(bookTitle) {
+ return bookTitle.length * 20; 
+};
+
+function writeBook(title, mainCharacter, genre) {
+  return {
+    title: title,
+    mainCharacter: mainCharacter,
+    genre: genre,
+    pageCount: calculatePageCount(title),
+  };
+};
+
+function editBook(book) {
+  book.pageCount = calculatePageCount(book.title) * .75;
+};
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
-}
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook,
+};
