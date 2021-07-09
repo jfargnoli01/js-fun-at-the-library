b151f33bbeaa981fd070b28ac5653458f69b1187
Juliana Fargnoli <fargnoli.jm@gmail.com>
2021-07-09 07:42:47 -0600

    Create dynamic shelf and unshelf functionality

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,22 @@
+function shelfBook(book, shelf) {
+  if(shelf.length < 3) {
+    shelf.unshift(book);
+  }
+};
+
+function unshelfBook(title, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if(shelf[i].title === title) {
+      shelf.splice(i, 1);
+    }
+  } 
+};
+
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
+  shelfBook,
+  unshelfBook,
   // listTitles,
   // searchShelf
 };
