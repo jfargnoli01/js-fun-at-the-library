77cf26391c4023353e819ab2613fc44f4b2eb6c0
Juliana Fargnoli <fargnoli.jm@gmail.com>
2021-07-09 16:32:51 -0600

    Complete checkoutBook functionality

--- src/Library.js
+++ src/Library.js
@@ -1,3 +1,5 @@
+const { shelfBook } = require("./shelf");
+
 function createLibrary(library) {
   return {
     name: library,
@@ -13,8 +15,18 @@ function addBook(library, book) {
   library.shelves[book.genre].push(book);
 };
 
+function checkoutBook(library, title, genre) {
+  for (var i = 0; i < library.shelves[genre].length; i++) {
+    if(library.shelves[genre][i].title === title) {
+      library.shelves[genre].splice(i, 1);
+      return `You have now checked out ${title} from the ${library.name}`;
+    } 
+  }
+  return `Sorry, there are currently no copies of ${title} available at the ${library.name}`;
+};
+
 module.exports = {
   createLibrary,
   addBook,
-  // checkoutBook
+  checkoutBook
 };
